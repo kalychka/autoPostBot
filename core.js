978e04b27d3973004a8e49fd425e7ea64496fd28
kalychka <randytaylor.fckoff@gmail.com>
2023-02-22 22:03:02 +0500

    fix core.js

--- core.js
+++ core.js
@@ -3,13 +3,13 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '5859407241:AAG66VfvBWGGzAt9yvawevtpSRM5CjuJjnk';
 
 const bot = new TelegramBot(token, {
-    webHook: {
-        port: 80
-    }
-    //polling: true
+    // webHook: {
+    //     port: 80
+    // }
+    polling: true
 });
 
-bot.setWebHook(`https://89.248.207.100/bot${token}`);
+//bot.setWebHook(`https://89.248.207.100/bot${token}`);
 
 const telegramAPI = `https://api.telegram.org/bot${token}/`;
 
