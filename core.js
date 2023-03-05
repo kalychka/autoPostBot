6345b4d62aab7d6ff9f04fac3be7ad71fc9dfa40
kalychka <randytaylor.fckoff@gmail.com>
2023-03-05 22:34:34 +0500

    fix core.js

--- core.js
+++ core.js
@@ -5,7 +5,12 @@ const token = process.env.BOT_TOKEN;
 
 const channelId = process.env.CHANNEL_NAME;
 
-const bot = new TelegramBot(token);
+const bot = new TelegramBot(token, {
+    webHook: {
+        port: 8443
+    }
+    //polling: true
+});
 
 bot.setWebHook(`https://prnaddiction.ru/bot${token}`);
 
