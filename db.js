c1f62eef96ea8b94505089bdd52869fe32704bef
kalychka <randytaylor.fckoff@gmail.com>
2023-03-20 17:25:41 +0500

    fix db.js

--- db.js
+++ db.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 
 const DB_PASS = process.env.DB_PASS;
 
-module.exports = new Sequelize(`postgres://kalychka:${DB_PASS}@ep-silent-field-988195.ap-southeast-1.aws.neon.tech/neondb?sslmode=require`)
+//module.exports = new Sequelize(`postgres://kalychka:${DB_PASS}@ep-silent-field-988195.ap-southeast-1.aws.neon.tech/neondb?sslmode=require`)
 
-// module.exports = new Sequelize(
-//     'yamei',
-//     'postgres',
-//     'kerosene', 
-//     {
-//         host: 'localhost',
-//         port: '5432',
-//         dialect: 'postgres'
-//     }
-// )
+module.exports = new Sequelize(
+    'yamei',
+    'postgres',
+    'kerosene', 
+    {
+        host: 'localhost',
+        port: '5432',
+        dialect: 'postgres'
+    }
+)
